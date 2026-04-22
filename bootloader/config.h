1c9623a64d699e270ece38dbcbf148d295e3754d
sinisterlukas-web <sinisterlukas@gmail.com>
2026-04-22 17:59:22 +0800

    Create config.h

---
+++ bootloader/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TARGET T8132   // The M4 chip ID
+#define USE_FB         // Enable the framebuffer for your logo
+#define BRINGUP        // Keep the minimal build active for early testing
+#define EARLY_PROXY_TIMEOUT 5 // Give users 5 seconds to open the shell
+
+#endif
